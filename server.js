8623048919e922684bfcdeb63cb75869f7b574f9
AshuM <maurya971@outlook.com>
2019-01-19 05:55:04 +0800

    initial commit with all

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
 
 var app = express();
+//view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.get("/", function(req, res) {
-  res.send("Hi Cyan!!!");
-});
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "public")));
+
 app.listen(8080, "127.0.0.1", function() {});
 
 module.exports = app;
